68cd3263d6d59a61b506cdf7811aafd49ce4bd29
Komron Khisomov <normok_9595@bk.ru>
2024-04-24 21:33:18 +0500

    gas

---
+++ src/Gas.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.19;
+
+contract Gas {
+    uint256 public i = 0;
+
+    function forever() public{
+        while(true){
+            i+= 1;
+        }
+    }
+}
