4655b9dfc38af413a8cb551060768ee24a4b1545
Komron Khisomov <normok_9595@bk.ru>
2024-04-24 21:20:27 +0500

    immutable

---
+++ src/Immutable.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.19;
+
+contract Immutable {
+    address public immutable MY_ADDRESS;
+    uint256 public immutable MY_UINT;
+
+    constructor(uint256 _myUint) {
+        MY_ADDRESS = msg.sender;
+        MY_UINT = _myUint;
+    }
+}
