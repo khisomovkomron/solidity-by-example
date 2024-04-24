2306791c9b15cb584c0cb37c75551827136a70df
Komron Khisomov <normok_9595@bk.ru>
2024-04-24 21:18:22 +0500

    constants

---
+++ src/Constants.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.19;
+
+contract Constants {
+
+    address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+    uint256 public constant MY_UINT = 123;
+}
