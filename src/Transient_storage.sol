0bf358fa3c99b470250f642bc01b998ab4f7a999
Komron Khisomov <normok_9595@bk.ru>
2024-04-26 01:16:38 +0500

    transient storage

---
+++ src/Transient_storage.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.19;
+
+// TODO
+
+
+// Used in Cancun version of EMV
+
+//  The new data location behaves as a key-value store similar to storage with the main difference being that data in transient storage is not permanent, 
+// but is scoped to the current transaction only, after which it will be reset to zero. 
