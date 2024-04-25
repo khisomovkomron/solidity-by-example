1ce73e561ca92776f1b9b34bcae2b30d6f44883f
Komron Khisomov <normok_9595@bk.ru>
2024-04-26 01:05:52 +0500

    dataLocations - storage/memory/calldata

---
+++ src/Data_Locations.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.19;
+
+
+/**
+* storage - variable is a state variable = store on blockchain - Storage is used to store data permanently on the blockchain.
+* memory - variable is in memory and it exists while a function is being called - Memory is used to store temporary data that is needed during the execution of a function. 
+* calldata - special data location that contains function arguments - Calldata is used to store function arguments that are passed in from an external caller. 
+* Calldata is used for function arguments that are passed in from an external caller
+*/
+
+contract DataLocations {
+    uint256[] public arr;
+    mapping(uint256 => address) map;
+
+    struct MyStruct{
+        uint256 foo;
+    }
+
+    mapping(uint256 => MyStruct) myStructs;
+
+    function f() public{
+        _f(arr, map, myStructs[1]);
+
+        MyStruct storage myStruct = myStructs[1];
+        MyStruct memory myMemStruct = MyStruct[0];
+    }
+
+    function _f(uint256[] storage _arr,
+                mapping(uint256 => address) storage _map,
+                MyStruct storage _myStruct
+            ) internal {
+                // do something with storage variables
+            }
+
+    function g(uint256[] memory _arr) public returns (uint256[] memory){
+        // do something with memory array
+    }
+
+    function h(uint256[] calldata _arr) external{
+        //do something with calldata array
+    }
+
+
+
+}
